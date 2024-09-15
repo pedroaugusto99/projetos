261dea972d629155e998d9bfbda1609a525e36ef
pedroaugusto99 <166179411+pedroaugusto99@users.noreply.github.com>
2024-09-15 00:23:18 -0300

    atualizações na landing page

--- rural-link/landing-page/src/scripts/script.js
+++ rural-link/landing-page/src/scripts/script.js
@@ -3,4 +3,64 @@ $(document).ready(function() {
         $('#mobile-menu').toggleClass('active');
         $('#mobile-btn').find('i').toggleClass('fa-x');
     });
+
+    const sections = $('section');
+    const navItems = $('.nav-item');
+
+    $(window).on('scroll', function(){
+        const header = $('header');
+        const scrollPosition = $(window).scrollTop() - header.outerHeight();
+
+        let activeSecctionIndex = 0;
+        
+        if (scrollPosition <= 0){
+            header.css('box-shadow', 'none')
+        } else {
+            header.css('box-shadow', '5px 1px 5px rgba(0, 0, 0, 0.1)');
+        }
+
+        sections.each(function(i) {
+            const section = $(this);
+            const sectionTop = section.offset().top - 96;
+            const sectionBottom = sectionTop + section.outerHeight();
+
+            if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+                activeSecctionIndex = i;
+                return false;
+            }
+        })
+
+        navItems.removeClass('active');
+        $(navItems[activeSecctionIndex]).addClass('active');
+    });
+
+    ScrollReveal().reveal('#banner img', {
+        origin: 'right', 
+        duration: 1000,
+        distance:'20%'
+    })
+
+    ScrollReveal().reveal('#cta', {
+        origin: 'left', 
+        duration: 2000,
+        distance:'20%'
+    });
+
+    ScrollReveal().reveal('.plano', {
+        origin: 'left', 
+        duration: 2000,
+        distance:'20%'
+    });
+
+    ScrollReveal().reveal('#testimonials-fazendeiro', {
+        origin: 'left', 
+        duration: 1000,
+        distance:'20%'
+    });
+
+    ScrollReveal().reveal('.feedback', {
+        origin: 'right', 
+        duration: 1000,
+        distance:'20%'
+    })
 });
